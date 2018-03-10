ed8d6f4eb5a6fa07f3894fd45f8069470843c957
TheOnlyJoey <joeyferweda@gmail.com>
2018-03-10 17:19:53 +0100

    Initial commit, does basic 2x2 and 4x4 level generation based on a seeded random (10 itterations)

---
+++ Objects/Player/Player.gd
@@ -0,0 +1,85 @@
+extends KinematicBody
+
+# Member variables
+var g = -9.8
+var vel = Vector3()
+const MAX_SPEED = 5
+const JUMP_SPEED = 7
+const ACCEL= 10
+const DEACCEL= 10
+const MAX_SLOPE_ANGLE = 30
+
+var view_sensitivity = 0.3
+var mouse_aim = true
+
+var running = false
+func _ready():
+	get_tree().set_pause(false)
+	set_process_input(true)
+
+func _enter_tree():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+
+func _exit_tree():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE);
+
+func _input(event):
+	if (mouse_aim):
+		if (event is InputEventMouseMotion):
+			var yaw = rad2deg(get_node(".").get_rotation().y);
+			var pitch = rad2deg(get_node(".").get_rotation().x);
+			
+			yaw = fmod(yaw - event.get_relative().x * view_sensitivity, 360);
+			pitch = max(min(pitch - event.get_relative().y * view_sensitivity, 90), -90);
+			
+			get_node(".").set_rotation(Vector3(deg2rad(pitch), deg2rad(yaw), 0));
+
+func _process(delta):
+	var dir = Vector3() # Where does the player intend to walk to
+	var cam_xform = $"./Camera".get_global_transform()
+	
+	if (Input.is_action_pressed("forward")):
+		dir += -cam_xform.basis[2]
+	if (Input.is_action_pressed("backward")):
+		dir += cam_xform.basis[2]
+	if (Input.is_action_pressed("left")):
+		dir += -cam_xform.basis[0]
+	if (Input.is_action_pressed("right")):
+		dir += cam_xform.basis[0]
+
+	if (Input.is_action_just_released("mousegrab")):
+		mouse_aim = !mouse_aim
+
+	if (Input.is_action_pressed("running")):
+		running = true
+	else:
+		running = false
+		
+	dir.y = 0
+	dir = dir.normalized()
+	
+	vel.y += delta*g
+	
+	var hvel = vel
+	hvel.y = 0
+	
+	var target = dir*MAX_SPEED
+	var accel
+	if (dir.dot(hvel) > 0):
+		accel = ACCEL
+	else:
+		accel = DEACCEL
+	
+	hvel = hvel.linear_interpolate(target, accel*delta)
+	
+	vel.x = hvel.x
+	vel.z = hvel.z
+	
+	if (running):
+		vel.x *= 1.15
+		vel.y *= 1.025
+		vel.z *= 1.15
+	vel = move_and_slide(vel,Vector3(0,1,0))
+	
+	if (is_on_floor() and Input.is_action_pressed("jump")):
+		vel.y = JUMP_SPEED
---
+++ Scenes/LevelGenerator.gd
@@ -0,0 +1,162 @@
+extends Spatial
+
+var roomlist2x2 = []
+var roomlist4x4 = []
+var corridorlist = []
+var room_coordinates = {}
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	var pool = preload("res://Objects/Rooms/RoomsAndCorridors.scn")
+	var pool_node = pool.instance()
+	#var path = pool.get_state().get_node_path(0)
+	#getallnodes(get_node(path.get_name(0)))
+	getallnodes(pool_node)
+	generateScene()
+	pass
+
+func getallnodes(node):
+	for N in node.get_children():
+		var node_name = N.get_name()
+		if N.get_child_count() > 0:
+			print("["+node_name+"]")
+			if "corridor" in node_name:
+				corridorlist.append(N)
+				print("Added ["+node_name+"] to corridorlist")
+			elif "room" in node_name:
+				#roomlist.append(N)
+				#print("Added ["+node_name+"] to roomlist")
+				
+				if "2x2" in node_name:
+					roomlist2x2.append(N)
+				if "4x4" in node_name:
+					roomlist4x4.append(N)
+
+			getallnodes(N)
+		else:
+			# Do something
+			print("- "+node_name)
+
+func loadAllAssets():
+	var dir = Directory.new()
+	if dir.open("res://Objects/Rooms") == OK:
+        dir.list_dir_begin()
+        var file_name = dir.get_next()
+        while (file_name != ""):
+            if dir.current_is_dir():
+                print("Found directory: " + file_name)
+            else:
+                print("Found file: " + file_name)
+            file_name = dir.get_next()
+
+var position = Vector3(0,0,0)
+var direction_offset_cor = Vector3(0,0,0)
+var direction_offset_room = Vector3(0,0,0)
+var rotation_offset = 0
+var size_offset = 16 #default 2x2
+var roomlist = null
+var direction = 0
+
+func recalculate_room_size(size):
+	if (size == 0): #2x2
+		size_offset = 16
+		roomlist = roomlist2x2
+	elif (size == 1): #4x4
+		size_offset = 32 - 8 #compensate for half of the size of the corridor?
+		roomlist = roomlist4x4
+	
+	if (direction == 0):
+		direction_offset_room = Vector3(size_offset,0,0)
+	elif (direction == 1):
+		direction_offset_room = Vector3(0,0,size_offset)
+	elif (direction == 2):
+		direction_offset_room = Vector3(-size_offset,0,0)
+	else:
+		direction_offset_room = Vector3(0,0,-size_offset)
+
+func generateScene():
+	var root = get_tree().get_root()
+	#Seeded
+	var random_seed = "itsarandomizerseed"
+	seed(random_seed.hash())
+	#Non-Seeded
+	#randomize()
+
+	var previous_room_offset = 16 #default 2x2
+	for x in range(0, 10):
+		direction = randi() % 4
+		var size = randi() % 2
+		roomlist = null
+		size_offset = 16 #default 2x2
+
+		if (size == 0): #2x2
+			size_offset = 16
+			roomlist = roomlist2x2
+		elif (size == 1): #4x4
+			size_offset = 32 - 8 #compensate for half of the size of the corridor?
+			roomlist = roomlist4x4
+		
+		if (direction == 0):
+			direction_offset_cor = Vector3(previous_room_offset,0,0)
+			direction_offset_room = Vector3(size_offset,0,0)
+			rotation_offset = deg2rad(90)
+		elif (direction == 1):
+			direction_offset_cor = Vector3(0,0,previous_room_offset)
+			direction_offset_room = Vector3(0,0,size_offset)
+			rotation_offset = deg2rad(0)
+		elif (direction == 2):
+			direction_offset_cor = Vector3(-previous_room_offset,0,0)
+			direction_offset_room = Vector3(-size_offset,0,0)
+			rotation_offset = deg2rad(-90)
+		else:
+			direction_offset_cor = Vector3(0,0,-previous_room_offset)
+			direction_offset_room = Vector3(0,0,-size_offset)
+			rotation_offset = deg2rad(0)
+		
+		var proposed_coord = position+(direction_offset_cor+direction_offset_room)
+		var proposed_coord_abs = proposed_coord + direction_offset_cor + direction_offset_room
+		var proposed_placement = AABB(proposed_coord_abs,corridorlist[0].get_aabb().size)
+		var proposed_allowed = false
+		
+		#if the proposed location is already in use, reverse the step and try again
+		if (proposed_coord in room_coordinates):
+			position += (direction_offset_cor+direction_offset_room)
+		else:
+			#check if there is a size collision for the new room
+			var finished = false
+			
+			if (!room_coordinates.empty()):
+				while (!finished):
+					for key in room_coordinates:
+						if (room_coordinates[key].intersects(proposed_placement)):
+							if (size <= 0):
+								position += (direction_offset_cor+direction_offset_room)
+								finished = true
+	
+							recalculate_room_size(size)
+							size -= 1
+						else:
+							finished = true
+							proposed_allowed = true
+	
+					finished = true
+			else:
+				proposed_allowed = true
+						
+		if (proposed_allowed):
+			#Corridors
+			position = position + Vector3(0,2,0) + direction_offset_cor
+			var cor = corridorlist[randi() % corridorlist.size()].duplicate()
+			cor.rotate(Vector3(0,1,0), rotation_offset)
+			cor.set_translation(position)
+			root.call_deferred("add_child", cor)
+
+			#Rooms
+			position = position + Vector3(0,-2,0) + direction_offset_room
+			var room = roomlist[randi() % roomlist.size()].duplicate()
+			room.set_translation(position)
+			room_coordinates[position] = room.get_aabb()
+			root.call_deferred("add_child", room)
+		
+		previous_room_offset = size_offset
