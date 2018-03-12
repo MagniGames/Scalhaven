b2e59aafd2f7b9e2dc2668d83c912baf4ae57209
TheOnlyJoey <joeyferweda@gmail.com>
2018-03-12 03:11:53 +0100

    Part of last commit

--- Objects/Player/Player.gd
+++ Objects/Player/Player.gd
@@ -13,9 +13,31 @@ var view_sensitivity = 0.3
 var mouse_aim = true
 
 var running = false
+
+var active_weapon = null
+var to_switch = null
+
+var inventory = {}
+var invUI = null
+
+var cube_gun = null
+var steamlock = null
+
 func _ready():
 	get_tree().set_pause(false)
 	set_process_input(true)
+	
+	#preload all weapons
+	#todo: make smarter
+	cube_gun = preload("res://Objects/Weapons/Cubegun.tscn").instance()
+	steamlock = preload("res://Objects/Weapons/Steamlock.tscn").instance()
+	
+	if (active_weapon != null):
+		to_switch = active_weapon
+	active_weapon = cube_gun
+	change_weapon()
+	invUI = get_node("UI/Inventory")
+	inventory["DefaultCube"] = 1
 
 func _enter_tree():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
@@ -46,6 +68,14 @@ func getallnodes(node):
 			if (N.has_method("get_aabb")):
 				print(N.get_aabb())
 
+func change_weapon():
+	if (active_weapon != null):
+		get_node("weapon").remove_child(to_switch)
+	get_node("weapon").add_child(active_weapon)
+
+func player_pickup(name, amount):
+	inventory[name] = amount
+
 var guntimer = -1
 func _process(delta):
 	var dir = Vector3() # Where does the player intend to walk to
@@ -68,10 +98,32 @@ func _process(delta):
 	else:
 		running = false
 
+	if (Input.is_action_pressed("slot1") or Input.is_action_pressed("slot2")):
+		invUI.show()
+		invUI.clear()
+		for i in inventory.keys():
+			invUI.add_item(i)
+			
+	if (Input.is_action_just_released("slot1")):
+		if (active_weapon != null):
+			to_switch = active_weapon
+		if ("DefaultCube" in inventory.keys()):
+			active_weapon = cube_gun
+			change_weapon()
+		invUI.hide()
+		
+	if (Input.is_action_just_released("slot2")):
+		if (active_weapon != null):
+			to_switch = active_weapon
+		if ("Steamlock" in inventory.keys()):
+			active_weapon = steamlock
+			change_weapon()
+		invUI.hide()
+
     # Firing the weapons
 	if Input.is_action_pressed("fire"):
 		if (guntimer < 0):
-			get_node("weapon").get_child(1).fire_bullet()
+			get_node("weapon").get_child(0).fire_bullet()
 		guntimer +=1
 
 	if (Input.is_action_just_released("debug")):
